7f1c6f918b8962ba7c2f2bd87141348baeaf5ad6
Prateek Singh <92904643+PrateekSinghRajput@users.noreply.github.com>
2021-12-06 20:12:37 +0530

    Add files via upload

---
+++ Fingerprint_And_Blutooth_based_door_lock.ino
@@ -0,0 +1,36 @@
+//Prateek
+//wwww.prateeks.in
+//https://www.youtube.com/c/JustDoElectronics/videos
+
+int value1;
+#define relay 13
+const int buzzer = 7; 
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(relay, OUTPUT);
+    pinMode(buzzer, OUTPUT); 
+    digitalWrite(relay, LOW);
+}
+void loop()
+{
+  Serial.print("Reading");
+  while(Serial.available()==0);
+  value1 = Serial.read();
+  Serial.print(value1);
+  if (value1==0)
+  { 
+    Serial.print("Unlocking");
+    digitalWrite(relay, LOW);
+    digitalWrite(buzzer, HIGH); 
+    delay(1000);        
+    digitalWrite(buzzer, LOW); 
+  }
+  if (value1==1)
+  {
+      digitalWrite(relay, HIGH);
+      Serial.print("Locking");
+      delay(3000); 
+      digitalWrite(relay, LOW);
+  }
+}
